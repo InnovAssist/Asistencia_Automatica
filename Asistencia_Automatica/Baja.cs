5aacd9af05d19d6e8f06d017f5ee5efec2ec6e52
Nikasawa <121456466+Nikasawa@users.noreply.github.com>
2024-09-26 08:52:05 -0300

    Inicio
    Primer commit del proyecto asistencia automatica con huella digital.
    Falta incorporar pantallas finalizadas de gestion y menu.

---
+++ Asistencia_Automatica/Baja.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Asistencia_Automatica
+{
+    public partial class Baja : Form
+    {
+        public Baja()
+        {
+            InitializeComponent();
+        }
+
+        private void Baja_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
