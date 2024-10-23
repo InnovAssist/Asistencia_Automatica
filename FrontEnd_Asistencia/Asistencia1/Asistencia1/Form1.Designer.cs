89f68176da52072f6a6b1129da4edc0255a5dd72
TinchoGs <martinmansilla615@gmail.com>
2024-10-22 22:13:16 -0300

    56
    uh

--- FrontEnd_Asistencia/Asistencia1/Asistencia1/Form1.Designer.cs
+++ FrontEnd_Asistencia/Asistencia1/Asistencia1/Form1.Designer.cs
@@ -28,36 +28,41 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button_Control = new System.Windows.Forms.Button();
+            this.Button_Gestion = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
+            // button_Control
             // 
-            this.button1.Location = new System.Drawing.Point(525, 120);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button_Control.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.button_Control.Location = new System.Drawing.Point(485, 114);
+            this.button_Control.Name = "button_Control";
+            this.button_Control.Size = new System.Drawing.Size(234, 95);
+            this.button_Control.TabIndex = 0;
+            this.button_Control.Text = "Control";
+            this.button_Control.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // Button_Gestion
             // 
-            this.button2.Location = new System.Drawing.Point(525, 231);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.Button_Gestion.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.Button_Gestion.Location = new System.Drawing.Point(485, 267);
+            this.Button_Gestion.Name = "Button_Gestion";
+            this.Button_Gestion.Size = new System.Drawing.Size(234, 95);
+            this.Button_Gestion.TabIndex = 1;
+            this.Button_Gestion.Text = "Gestion";
+            this.Button_Gestion.UseVisualStyleBackColor = true;
+            this.Button_Gestion.Click += new System.EventHandler(this.button2_Click);
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(191, 159);
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(110, 114);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.Size = new System.Drawing.Size(318, 248);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
@@ -66,10 +71,11 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Button_Gestion);
+            this.Controls.Add(this.button_Control);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
@@ -79,8 +85,8 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button_Control;
+        private System.Windows.Forms.Button Button_Gestion;
         private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
